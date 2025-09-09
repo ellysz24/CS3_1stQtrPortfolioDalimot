db26a934eb22d736e35ba56bc1107685d3e319c5
ellysz24 <b2029ketdalimot@pshs.edu.ph>
2025-09-09 14:25:05 +0800

    29

---
+++ ohyes.js
@@ -0,0 +1,20 @@
+
+
+grades = [99,100,97,98,96]
+arr = grades
+grades[0] = 1000
+console.log(arr[0])
+for (let i = 0; i < grades.length; i++) {
+    console.log(grades[i])
+}
+
+for(let grade of grades) {
+    console.log(grades)
+}
+
+grades.forEach(print_grades)
+
+function print_grades(grades) {
+    console.log(grades)
+}
+
